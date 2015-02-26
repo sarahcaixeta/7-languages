ecdbfe928c0ed0246c6e9c0411c9f4de5b624be6
Sarah Caixeta <caixeta.sarah@gmail.com>
2015-02-26 20:15:14 -0300

    Adds line number to grep

--- ruby/grep.rb
+++ ruby/grep.rb
@@ -1,9 +1,9 @@
 def grep file_path, pattern
   regex = Regexp.new(pattern)
   file = File.open(file_path).read
-  file.each_line do |line|
-    puts line if (regex =~ line)
+  file.each_line.each_with_index do |line, index|
+    puts "Line #{index}: #{line}" if (regex =~ line)
   end
 end
 
-grep('example.rb', 'a')
+grep('example.rb', 'p')
