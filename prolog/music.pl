a62102adffaae45faae8e23f78d24f340e1bbaea
Sarah Caixeta <caixeta.sarah@gmail.com>
2015-04-01 09:22:46 -0300

    Knowledge base for musicians and instruments.

---
+++ prolog/music.pl
@@ -0,0 +1,6 @@
+musician('Jimi Hendrix', guitar).
+musician('B.B. King', guitar).
+musician('Keith Richards', guitar).
+musician('John Bonham', drums).
+musician('Dave Grohl', drums).
+musician('John Deacon', bass).
