1ace57d7f2d96eb88a0b0279860d1fe7d763f4d1
Sarah Caixeta <caixeta.sarah@gmail.com>
2015-04-09 19:29:48 -0300

    rule to validate if all the queens are in valid positions

--- prolog/eight_queens.pl
+++ prolog/eight_queens.pl
@@ -2,5 +2,10 @@ valid_queen((Row, Col)) :-
   Range = [1, 2, 3, 4, 5, 6, 7, 8],
   member(Row, Range), member(Col, Range).
 
+valid_board([]).
+valid_board([Head|Tail]) :-
+  valid_queen(Head),
+  valid_board(Tail).
+
 eight_queens(List) :-
   length(List, 8).
