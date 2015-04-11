b9891253b5c4e1ab96406a3e896828430de32a03
Sarah Caixeta <caixeta.sarah@gmail.com>
2015-04-10 23:17:29 -0300

    Eight queens solution

--- prolog/eight_queens.pl
+++ prolog/eight_queens.pl
@@ -8,16 +8,16 @@ valid_board([Head|Tail]) :-
   valid_board(Tail).
 
 rows([], []).
-rows([(Row, _) | QueensTail], [Row|RowTail]) :-
+rows([(Row, _) | QueensTail], [Row|RowsTail]) :-
   rows(QueensTail, RowsTail).
 
 cols([], []).
-cols([(_, Col) | QueensTail], [Col, ColsTail]) :-
+cols([(_, Col) | QueensTail], [Col|ColsTail]) :-
   cols(QueensTail, ColsTail).
 
 diagonal1([], []).
 diagonal1([(Row, Col) | QueensTail], [Diagonal| DiagonalsTail]) :-
-  Diagonal is Col - Row;
+  Diagonal is Col - Row,
   diagonal1(QueensTail, DiagonalsTail).
 
 diagonal2([], []).
@@ -25,5 +25,16 @@ diagonal2([(Row, Col) | QueensTail], [Diagonal|DiagonalsTail]) :-
   Diagonal is Col + Row,
   diagonal2(QueensTail, DiagonalsTail).
 
-eight_queens(List) :-
-  length(List, 8).
+eight_queens(Board) :-
+  length(Board, 8),
+  valid_board(Board),
+
+  rows(Board, Rows),
+  cols(Board, Cols),
+  diagonal1(Board, Diags1),
+  diagonal2(Board, Diags2),
+
+  all_different(Rows),
+  all_different(Cols),
+  all_different(Diags1),
+  all_different(Diags2).
