9f46259eeb7653116ffbcff53f5b89a87e537748
Sarah Caixeta <caixeta.sarah@gmail.com>
2015-04-10 21:22:47 -0300

    Adds validation for rows, columns and diagonals

--- prolog/eight_queens.pl
+++ prolog/eight_queens.pl
@@ -7,5 +7,23 @@ valid_board([Head|Tail]) :-
   valid_queen(Head),
   valid_board(Tail).
 
+rows([], []).
+rows([(Row, _) | QueensTail], [Row|RowTail]) :-
+  rows(QueensTail, RowsTail).
+
+cols([], []).
+cols([(_, Col) | QueensTail], [Col, ColsTail]) :-
+  cols(QueensTail, ColsTail).
+
+diagonal1([], []).
+diagonal1([(Row, Col) | QueensTail], [Diagonal| DiagonalsTail]) :-
+  Diagonal is Col - Row;
+  diagonal1(QueensTail, DiagonalsTail).
+
+diagonal2([], []).
+diagonal2([(Row, Col) | QueensTail], [Diagonal|DiagonalsTail]) :-
+  Diagonal is Col + Row,
+  diagonal2(QueensTail, DiagonalsTail).
+
 eight_queens(List) :-
   length(List, 8).
