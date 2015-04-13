afd71ccb6261a0e5c8210a1547f1b2b667a0cb95
Sarah Caixeta <caixeta.sarah@gmail.com>
2015-04-13 18:44:44 -0300

    Considers the 3x3 squares

--- prolog/sudoku-9x9.pl
+++ prolog/sudoku-9x9.pl
@@ -31,8 +31,19 @@ sudoku(Puzzle, Solution) :-
   Col8 = [S18, S28, S38, S48, S58, S68, S78, S88, S98],
   Col9 = [S19, S29, S39, S49, S59, S69, S79, S89, S99],
 
+  Square1 = [S11, S12, S13, S21, S22, S23, S31, S32, S33],
+  Square2 = [S14, S15, S16, S24, S25, S26, S34, S35, S36],
+  Square3 = [S17, S18, S19, S27, S28, S29, S37, S38, S39],
+  Square4 = [S41, S42, S43, S51, S52, S53, S61, S62, S63],
+  Square5 = [S44, S45, S46, S54, S55, S56, S64, S65, S66],
+  Square6 = [S47, S48, S49, S57, S58, S59, S67, S68, S69],
+  Square7 = [S71, S72, S73, S81, S82, S83, S91, S92, S93],
+  Square8 = [S74, S75, S76, S84, S85, S86, S94, S95, S96],
+  Square9 = [S77, S78, S79, S87, S88, S89, S97, S98, S99],
+
   valid([Row1, Row2, Row3, Row4, Row5, Row6, Row7, Row8, Row9,
-    Col1, Col2, Col3, Col4, Col5, Col6, Col7, Col8, Col9]).
+    Col1, Col2, Col3, Col4, Col5, Col6, Col7, Col8, Col9,
+    Square1, Square2, Square3, Square4, Square5, Square6, Square7, Square7, Square8, Square9]).
 
 valid([]).
 valid([Head|Tail]) :-
