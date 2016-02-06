51c9fc57e736be7511d61489984aa0c1689cb281
Sarah Caixeta <caixeta.sarah@gmail.com>
2016-02-06 19:38:13 -0200

    collection type in clojure

--- clojure/day-one/src/day_one/core.clj
+++ clojure/day-one/src/day_one/core.clj
@@ -3,9 +3,9 @@
 (defn big [word n] 
   (< (count word) n))
 
+(defn collection-type [col] 
+  (if (= (type col) clojure.lang.PersistentList) :list 
+    (if (= (type col) clojure.lang.PersistentArrayMap) :map 
+      (if (= (type col) clojure.lang.PersistentVector) :vector
+        :other))))
 
-
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
