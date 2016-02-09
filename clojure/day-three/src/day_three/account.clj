ea99d7b64b73aa1b2d6fcbd8330f41dbe7e369f8
Sarah Caixeta <caixeta.sarah@gmail.com>
2016-02-09 11:09:19 -0200

    Day 3 - vector of accounts with ref

---
+++ clojure/day-three/src/day_three/account.clj
@@ -0,0 +1,14 @@
+(ns day_three.account)
+
+(def accounts (ref [0, 0]))
+
+(defn change-balance [id operation amount]
+	(dosync
+		(alter accounts assoc id
+			(operation (nth @accounts id) amount))))
+
+(defn credit [id amount]
+		(change-balance id + amount))
+
+(defn debit [id amount]
+		(change-balance id - amount))
